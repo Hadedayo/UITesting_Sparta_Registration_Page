5509c17a097ac1bb0874359aa03f13240ffc171c
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-12-08 15:42:34 +0000

    Creating methods to access elements on the page using id, name and css

--- Solution_code/sparta_registration_solution.rb
+++ Solution_code/sparta_registration_solution.rb
@@ -23,7 +23,45 @@ class Registration_Selenium
     @chrome_driver.find_element(:id, 'lastName').send_keys(text)
   end
 
+  def input_age(text)
+   @chrome_driver.find_element(:css, 'input[type="number"]').send_keys(text)
+  end
 
+  def input_date_of_birth(text)
+    @chrome_driver.find_element(:name, 'dob').send_keys(text)
+  end
+
+  def select_gender_female
+    @chrome_driver.find_element(:css, 'label[for=customRadioInline2]').click
+  end
+
+  def input_degree(text)
+    @chrome_driver.find_element(:css, 'input[placeholder="Enter Degree"]').send_keys(text)
+  end
+
+  def select_university
+    dropDownMenu = @chrome_driver.find_element(:id, 'inputUni')
+    option = Selenium::WebDriver::Support::Select.new(dropDownMenu)
+    option.select_by(:text, 'University of Oxford')
+  end
+
+  def input_address(text)
+    @chrome_driver.find_element(:id, 'inputAddress').send_keys(text)
+  end
+
+  def input_address_2(text)
+    @chrome_driver.find_element(:id, 'inputAddress2').send_keys(text)
+  end
+
+  def input_city(text)
+    @chrome_driver.find_element(:id, 'inputCity').send_keys(text)
+  end
+
+  def select_county
+    dropDownMenu = @chrome_driver.find_element(:id, 'inputCounty')
+    option = Selenium::WebDriver::Support::Select.new(dropDownMenu)
+    option.select_by(:text, 'Bristol')
+  end
 
 end
 
@@ -31,6 +69,14 @@ test = Registration_Selenium.new
 test.visit_registration_site
 sleep 2
 test.input_first_name_field('Hassanat')
-sleep 2
 test.input_last_name_field('Ade')
-sleep 5
+test.input_age('21')
+test.input_date_of_birth('08/12/2018')
+test.select_gender_female
+test.input_degree('Chemical Engineering')
+test.select_university
+test.input_address('23 Bank street')
+test.input_address_2('Flat 3')
+test.input_city('London')
+test.select_county
+sleep 3
